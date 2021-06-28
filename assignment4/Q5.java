3e4fdfca28d32f09d9e582cb11686a2dfb1cad26
mayuri shinde <mayuris0199@gmail.com>
2021-06-28 19:04:30 +0900

    code updated

--- assignment4/Q5.java
+++ assignment4/Q5.java
@@ -1,37 +1,26 @@
-/*
-    Accept 5 numbers in an array such that the square is stored in the next elements of the array.
-     [5 , 25, 12 , 144 , 3 , 9] 
-*/
+
 import java.util.Scanner;
- public class Q5 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        int n,i,oddind=1;
-        System.out.print("Enter array size= ");
-        n= sc.nextInt();
-        int arr[] = new int[n]; 
-        int square = arr[0];
-        System.out.println("Enter array element= ");
-        while(true)
-        {
-            while(oddind <n && arr[oddind]%2 == 1)
-                   oddind +=2;
-
-            break;       
-        }
-
-        for( i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
-        } 
-        for(int num: arr)
-        {
-             square = num *num; 
-             arr[oddind] = square ;
-        }
-        System.out.println(arr[oddind]);
-        
-        sc.close();
-    }
+public class Q5{
+     public static void main(String[] args)
+     {
+         Scanner sc = new Scanner(System.in);
+         int n;
+         int arr[] = new int[10];
+         
+         System.out.println("Enter the element in array");
+         for(int i=0;i<10;i++)
+         {
+             if(i%2==0)
+             {
+                 n=sc.nextInt();
+                 arr[i]=n;
+                 arr[i+1]=n*n;
+             }
+         }
+         System.out.println("Square of array element");
+         for(int j=0;j<10;j=j+2)
+         {
+             System.out.println(arr[j]+"  "+arr[j+1]);
+         }
+     }
 }
